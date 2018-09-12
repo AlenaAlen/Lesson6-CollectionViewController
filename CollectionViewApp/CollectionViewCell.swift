3f116bea943dc9beb440ffc27b9f74e575e4759b
Alena Alen <alenak.27.08@gmail.com>
2018-09-12 18:39:28 +0500

    add
    add

---
+++ CollectionViewApp/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewApp
+//
+//  Created by Алена Кропотова on 12.09.2018.
+//  Copyright © 2018 Alen. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
---
+++ CollectionViewApp/CollectionViewController.swift
@@ -0,0 +1,80 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewApp
+//
+//  Created by Алена Кропотова on 12.09.2018.
+//  Copyright © 2018 Alen. All rights reserved.
+//
+
+import UIKit
+
+
+
+class CollectionViewController: UICollectionViewController {
+
+    
+    let arrayImage = [#imageLiteral(resourceName: "pet"),#imageLiteral(resourceName: "pet-1"),#imageLiteral(resourceName: "pet-2"),#imageLiteral(resourceName: "pet-3"),#imageLiteral(resourceName: "pet-4"),#imageLiteral(resourceName: "pet-5"),#imageLiteral(resourceName: "pet-6")]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+   
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return arrayImage.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? CollectionViewCell
+    
+        cell?.imageView.image  = arrayImage[indexPath.row]
+        return cell!
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+
+}
